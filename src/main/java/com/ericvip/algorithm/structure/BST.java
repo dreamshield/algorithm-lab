173b818c13aa87970c2a1a60a4767622a7dfc536
EricZhang <root@ericvip.com>
2020-12-03 08:38:19 +0800

    二叉搜索树

---
+++ src/main/java/com/ericvip/algorithm/structure/BST.java
@@ -0,0 +1,385 @@
+package com.ericvip.algorithm.structure;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.Consumer;
+
+/**
+ * 二叉查找树：每一个节点都含有一个Comparable的键(以及相关联的值)且每个节点的键都大于其左子树中的任意节点的键
+ * 而小于其右子树中任意节点的键。
+ *
+ * @author EricZhang
+ * @date 2020/11/23
+ */
+public class BST<K extends Comparable<? super K>, V> implements BinaryTree<K, V> {
+
+    /**
+     * 根节点
+     */
+    private Node<K, V> root;
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    @Override
+    public void preOrderTraverse() {
+        preOrderTraverse(root);
+    }
+
+    /**
+     * 先序遍历指定子树序列
+     * 思路:
+     * step1: 一路向左遍历节点，并处理当前节点，并入栈
+     * step2: 左节点处理完成，开始处理各左节点(之前入栈的节点)的右子树
+     *
+     * @param root 子树根节点
+     */
+    private void preOrderTraverse(Node<K, V> root) {
+        Deque<Node<K, V>> stack = new ArrayDeque<>();
+        Node<K, V> node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                doSomething(System.out::println, node.getValue());
+                stack.push(node);
+                node = node.getLeft();
+            }
+            if (!stack.isEmpty()) {
+                node = stack.pop().getRight();
+            }
+        }
+    }
+
+    @Override
+    public void inOrderTraverse() {
+        inOrderTraverse(root);
+    }
+
+    /**
+     * 中序遍历指定子树序列
+     * 思路：
+     * step1: 一路向左遍历节点并入队列
+     * step2: 到达最左端后，队列pop该节点并进行处理
+     * step3: 处理完继续处理该节点的右子树
+     *
+     * @param root 当前子树的根节点
+     */
+    private void inOrderTraverse(Node<K, V> root) {
+        Deque<Node<K, V>> stack = new ArrayDeque<>();
+        Node<K, V> node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node.getLeft());
+                node = node.getLeft();
+            }
+
+            if (!stack.isEmpty()) {
+                node = stack.pop();
+                doSomething(System.out::println, node.getValue());
+                node = node.getRight();
+            }
+        }
+    }
+
+    @Override
+    public void postOrderTraverse() {
+        postOrderTraverse(root);
+    }
+
+    /**
+     * 后续遍历指定子序列
+     * 思路：
+     * step1: 一路向左遍历左节点并入栈
+     * step2: 每次进行节点处理前，先判断该节点的右子树是否均处理完成
+     * (包括两种情况：①右子树为空；②右子树遍历完成，即lastVisit = node.getRight())
+     * step3: 如果右子树未遍历完成，先遍历右子树
+     * 注：lastVisit初始值设置为root的原因
+     *
+     * @param root 根节点
+     */
+    private void postOrderTraverse(Node<K, V> root) {
+        Deque<Node<K, V>> stack = new ArrayDeque<>();
+        Node<K, V> node = root;
+        Node<K, V> lastVisit = null;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.getLeft();
+            }
+            // 处理当前节点前，先判断右子树情况
+            // 不满足处理当前节点条件时，当前节点还要保留在栈中，因为还没有处理，所以不能pop
+            node = stack.peek();
+            if (node.getRight() == null || node.getRight() == lastVisit) {
+                doSomething(System.out::println, node.getValue());
+                stack.pop();
+                lastVisit = node;
+                node = null;
+            } else {
+                node = node.getRight();
+            }
+        }
+    }
+
+
+    private void doSomething(Consumer<V> c, V value) {
+        c.accept(value);
+    }
+
+    /**
+     * 获取指定节点对应子树中的节点总数
+     *
+     * @param node 指定的节点
+     * @return 节点数
+     */
+    private int size(Node<K, V> node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.getN();
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    /**
+     * 在指定子树中插入键值对
+     *
+     * @param node  指定子树的根节点
+     * @param key   键
+     * @param value 值
+     * @return 新插入的节点(返回值只有在新插入节点时才有意义)
+     */
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) {
+            return new Node<>(key, value, 1);
+        }
+        int cmp = node.getKey().compareTo(key);
+        // 左子节点
+        if (cmp < 0) {
+            node.setLeft(put(node.getLeft(), key, value));
+        }
+        // 右子节点
+        else if (cmp > 0) {
+            node.setRight(put(node.getRight(), key, value));
+        }
+        // 当前节点
+        else {
+            node.setValue(value);
+        }
+        node.setN(node.getLeft().getN() + node.getRight().getN() + 1);
+        return node;
+    }
+
+    /**
+     * 查主指定的键
+     * 实现方法：循环
+     *
+     * @param key 指定键
+     * @return 查找结果
+     */
+    @Override
+    public V get(K key) {
+        Node<K, V> node = root;
+        while (node != null) {
+            int cmp = node.getKey().compareTo(key);
+            if (cmp == 0) {
+                return node.getValue();
+            } else if (cmp < 0) {
+                node = node.getRight();
+            } else {
+                node = node.getLeft();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 在指定的子树中查找对应键
+     * 实现方法：递归
+     *
+     * @param node 指定子树的根节点
+     * @param key  查找的键
+     * @return 查找结果
+     */
+    public V get(Node<K, V> node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = node.getKey().compareTo(key);
+        // 左节点
+        if (cmp < 0) {
+            return get(node.getLeft(), key);
+        }
+        // 右节点
+        else if (cmp > 0) {
+            return get(node.getRight(), key);
+        }
+        // 当前节点
+        else {
+            return node.getValue();
+        }
+    }
+
+    @Override
+    public void delete(K key) {
+
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public K minKey() {
+        Node<K, V> node = minNode(root);
+        if (node != null) {
+            return node.getKey();
+        }
+        return null;
+    }
+
+    /**
+     * 注：最小节点一定存在于左子树中
+     */
+    @Override
+    public Node<K, V> minNode(Node<K, V> root) {
+        Node<K, V> node = root;
+        while (node != null) {
+            if (node.getLeft() != null) {
+                node = node.getLeft();
+            } else {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public K max() {
+        Node<K, V> maxNode = maxNode(root);
+        if (maxNode != null) {
+            return maxNode.getKey();
+        }
+        return null;
+    }
+
+    /**
+     * 最大节点一定存在于右子树中
+     */
+    @Override
+    public Node<K, V> maxNode(Node<K, V> root) {
+        Node<K, V> node = root;
+        while (node != null) {
+            if (node.getRight() != null) {
+                node = node.getRight();
+            } else {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public K floorKey(K key) {
+        Node<K, V> floorNode = floorNode(root, key);
+        if (floorNode != null) {
+            return floorNode.getKey();
+        }
+        return null;
+    }
+
+    /**
+     * 小于等于key的最大值
+     * 思路：
+     * ①：当前节点等于指定的key，当前节点就是目标节点;
+     * ②：当前节点小于指定的key(说明小于指定key的最大值在右子树)
+     * - 如果当前节点右子树为空，或者右子树大于指定的key，那么当前节点就是目标节点
+     * - 否则目标节点一定在右子树，继续遍历右子树
+     * ③：当前节点大于指定的key(说明目标节点在左子树)
+     * 需要向左一直遍历每个子树，同时对每个子树的根节点应用条件①与②进行判断
+     */
+    @Override
+    public Node<K, V> floorNode(Node<K, V> root, K key) {
+        Node<K, V> node = root;
+        int cmp;
+        while (node != null) {
+            cmp = node.getKey().compareTo(key);
+            if (cmp == 0) {
+                return node;
+            } else if (cmp > 0) {
+                node = node.getLeft();
+            } else {
+                if (node.getRight() == null || node.getRight().getKey().compareTo(key) > 0) {
+                    return node;
+                } else {
+                    node = node.getRight();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 大于等于指定key的最小值的节点
+     * 思路：
+     * ①：当前节点等于指定的key, 当前节点就是目标节点;
+     * ②：当前节点大于指定的key(说明大于key的最小值在左子树)
+     * - 如果当前节点的做节点为空，或者当前节点的左节点小于指定的key，那么当前节点就是目标节点；
+     * - 否则，最小节点一定在当前节点的左子树；
+     * ③：当前节点小于指定的key(说明大于key的最小值在右子树)
+     * 需要向右一直遍历，每个子树都通过条件①与②来判断节点。
+     */
+    @Override
+    public Node<K, V> ceilingNode(Node<K, V> root, K key) {
+        Node<K, V> node = root;
+        int cmp;
+        while (node != null) {
+            cmp = node.getKey().compareTo(key);
+            if (cmp == 0) {
+                return node;
+            } else if (cmp > 0) {
+                if (node.getLeft() == null || node.getLeft().getKey().compareTo(key) < 0) {
+                    return node;
+                } else {
+                    node = node.getLeft();
+                }
+            } else {
+                node = node.getRight();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public K ceilingKey(K key) {
+        Node<K, V> node = ceilingNode(root, key);
+        if (node != null) {
+            return node.getKey();
+        }
+        return null;
+    }
+
+    @Override
+    public int rank(K key) {
+        return 0;
+    }
+
+    @Override
+    public K select(int r) {
+        return null;
+    }
+}
--- src/main/java/com/ericvip/algorithm/structure/BinaryTree.java
+++ src/main/java/com/ericvip/algorithm/structure/BinaryTree.java
@@ -1,60 +1,238 @@
 package com.ericvip.algorithm.structure;
 
-import java.util.Iterator;
-
 /**
+ * 以下内容是对二叉树相关的定义与性质的整理：
+ * 节点深度：对于任意节点n,n的深度为从根到n的唯一路径长，根的深度为0。
+ * 节点高度：对于任意节点n,n的高度为从n到一片树叶的最长路径长，所有树叶的高度为0。
+ * 树的深度=树的高度(每个的定义不同，可能出现大小为1的差值)。
+ * 树的深度(深度比高度更能提现一棵向下生长的二叉树)：树中所有节点的最大深度，称为树的深度。特别的根节点的深度为0。
+ * 度：树中任意节点的子树数目，称为该节点的度。
+ * 树的度：一棵树中子树度的最大值。
+ * 路径：由树中k+1节点通过树边首尾衔接而构成的序列{(v0, v1),(v1, v2), …, (vk-1 , vk) | k ≥ 0}，称作树中长度为k的一条路径(Path)。
+ * 二叉树：是指每个节点最多有两个子树的有序树，通常将其两个子树的根分别称作"左子树"和"右子树"。
+ * 满二叉树：除了叶子节点外，每一个节点都有左右子叶，并且叶子节点都处在最底层的二叉树。
+ * 完全二叉树：只有在最下面两层节点的度小于2，并且最下面一层的节点都集中在该层最左边的若干位置的二叉树。
+ * 二叉树的性质：
+ * 性质①：在二叉树中，深度为k的节点不超过2^k个。
+ * 性质②：深度为h的二叉树，最多包含2^(h+1)-1个节点。
+ * 性质③：由n个节点构成的二叉树，深度至少为floor(log2(n))。
+ * 性质④：对于任意一棵二叉树，如果叶子节点数为n0，且度2的节点数为n2，则n0=n2+1。
+ * 性质⑤：由n个节点构成的完全二叉树，高度为floor(log2(n))。
+ * 性质⑥：由固定数目的节点组成的所有二叉树中，完全二叉树的高度最低。
+ * 性质⑦：对于一个有n个节点构成的完全二叉树，且节点采用顺序存储的方式存储，则节点之间存在如下关系：
+ * A：如果i=1，则节点i为根，无父节点；如果i>1，则其父节点为floor(i/2);
+ * B：如果2i<=n，则其左子树的根节点为2i；如果2i>n，则无左子树；
+ * C：如果2i+1<=n，则其右子树的根节点为2i+1；如果2i+1>n，则无右子树。
+ *
  * @author EricZhang
  * @date 2020/1/19
- * @time 8:34 AM
  */
-public interface BinaryTree {
+public interface BinaryTree<K extends Comparable<? super K>, V> {
+
     /**
-     * 以下内容是对二叉树相关的定义与性质的整理：
-     * 节点深度：对于任意节点n,n的深度为从根到n的唯一路径长，根的深度为0。
-     * 节点高度：对于任意节点n,n的高度为从n到一片树叶的最长路径长，所有树叶的高度为0。
-     * 树的深度=树的高度(每个的定义不同，可能出现大小为1的差值)。
-     * 树的深度(深度比高度更能提现一棵向下生长的二叉树)：树中所有节点的最大深度，称为树的深度。特别的根节点的深度为0。
-     * 度：树中任意节点的子树数目，称为该节点的度。
-     * 树的度：一棵树中子树度的最大值。
-     * 路径：由树中k+1节点通过树边首尾衔接而构成的序列{(v0, v1),(v1, v2), …, (vk-1 , vk) | k ≥ 0}，称作树中长度为k的一条路径(Path)。
-     * 二叉树：是指每个节点最多有两个子树的有序树，通常将其两个子树的根分别称作"左子树"和"右子树"。
-     * 满二叉树：除了叶子节点外，每一个节点都有左右子叶，并且叶子节点都处在最底层的二叉树。
-     * 完全二叉树：只有在最下面两层节点的度小于2，并且最下面一层的节点都集中在该层最左边的若干位置的二叉树。
-     * 二叉树的性质：
-     * 性质①：在二叉树中，深度为k的节点不超过2^k个。
-     * 性质②：深度为h的二叉树，最多包含2^(h+1)-1个节点。
-     * 性质③：由n个节点构成的二叉树，深度至少为floor(log2(n))。
-     * 性质④：对于任意一棵二叉树，如果叶子节点数为n0，且度2的节点数为n2，则n0=n2+1。
-     * 性质⑤：由n个节点构成的完全二叉树，高度为floor(log2(n))。
-     * 性质⑥：由固定数目的节点组成的所有二叉树中，完全二叉树的高度最低。
-     * 性质⑦：对于一个有n个节点构成的完全二叉树，且节点采用顺序存储的方式存储，则节点之间存在如下关系：
-     * A：如果i=1，则节点i为根，无父节点；如果i>1，则其父节点为floor(i/2);
-     * B：如果2i<=n，则其左子树的根节点为2i；如果2i>n，则无左子树；
-     * C：如果2i+1<=n，则其右子树的根节点为2i+1；如果2i+1>n，则无右子树。
+     * 二叉树节点
+     *
+     * @param <K> 键
+     * @param <V> 值
      */
+    class Node<K extends Comparable<? super K>, V> {
+        /* 键 */
+        private K key;
+        /* 值 */
+        private V value;
+        /* 左子树 */
+        private Node<K, V> left;
+        /* 右子树 */
+        private Node<K, V> right;
+        /* 以该节点为根的子树中的节点总数 */
+        private int n;
+
+        public Node(K key, V value, int n) {
+            this.key = key;
+            this.value = value;
+            this.n = n;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Node<K, V> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<K, V> left) {
+            this.left = left;
+        }
+
+        public Node<K, V> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<K, V> right) {
+            this.right = right;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+    }
+
+    /**
+     * 前序遍历
+     * 注：自身节点 -> 左子树 -> 右子树
+     */
+    void preOrderTraverse();
+
+    /**
+     * 中序遍历
+     * 注：左子树 -> 自身节点 -> 右子树
+     */
+    void inOrderTraverse();
+
+    /**
+     * 后序遍历
+     * 注：左子树 -> 右子树 -> 自身节点
+     */
+    void postOrderTraverse();
 
     /**
      * 判断树是否为空
+     *
      * @return true:空，false:非空
      */
     boolean isEmpty();
 
     /**
      * 树的大小(节点数量)
-     * @return
+     * 表中键值对的数量
      */
     int size();
 
     /**
      * 数的高度
-     * @return
      */
     int height();
 
-//    Iterator
+    /**
+     * 将键值对存入二叉树中
+     *
+     * @param key   键
+     * @param value 值
+     */
+    void put(K key, V value);
 
+    /**
+     * 获取指定键对应的值，若key不存在返回null
+     *
+     * @param key 指定键
+     * @return 返回值
+     */
+    V get(K key);
 
+    /**
+     * 从表中删去键key对应的节点
+     *
+     * @param key 指定键
+     */
+    void delete(K key);
 
+    /**
+     * 指定键key是否存在与表中
+     *
+     * @param key 指定的键
+     * @return 判断结果
+     */
+    boolean contains(K key);
 
+    /**
+     * 最小的键
+     */
+    K minKey();
+
+    /**
+     * 返回指定子树的最小节点
+     *
+     * @param root 指定子树的根节点
+     * @return 最小节点
+     */
+    Node<K, V> minNode(Node<K, V> root);
+
+    /**
+     * 最大的键
+     */
+    K max();
+
+    /**
+     * 返回指定子树的最大节点
+     *
+     * @param root 指定子树的根节点
+     * @return 最大节点
+     */
+    Node<K, V> maxNode(Node<K, V> root);
+
+    /**
+     * 小于等于key的最大键
+     *
+     * @param key 指定的key
+     */
+    K floorKey(K key);
+
+    /**
+     * 指定子树中小于等于key的最大节点
+     *
+     * @param root 子树的根节点
+     * @param key  指定key
+     * @return 找到的节点
+     */
+    Node<K, V> floorNode(Node<K, V> root, K key);
+
+    /**
+     * 大于等于key的最小键
+     *
+     * @param key 指定的键
+     */
+    K ceilingKey(K key);
+
+    /**
+     * 指定子树中大于等于key的最小节点
+     *
+     * @param root 子树的根节点
+     * @param key  指定key
+     * @return 找到的节点
+     */
+    Node<K, V> ceilingNode(Node<K, V> root, K key);
+
+    /**
+     * 小于等于key的键的数量
+     *
+     * @param key 指定的key
+     * @return 键的数量
+     */
+    int rank(K key);
+
+    /**
+     * 排名为r的键
+     *
+     * @param r 指定的排名
+     * @return 对应的键
+     */
+    K select(int r);
 
 }
